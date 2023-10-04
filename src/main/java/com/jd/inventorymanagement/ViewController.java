0e6d0974285c2e61c1396a8519917f1e4d8e7c60
James Dunaway <jamesdunaway95@gmail.com>
2023-10-04 15:26:21 -0700

    Created GUI mockup using scenebuilder.

--- src/main/java/com/jd/inventorymanagement/ViewController.java
+++ src/main/java/com/jd/inventorymanagement/ViewController.java
@@ -1,8 +1,82 @@
 package com.jd.inventorymanagement;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 public class ViewController {
+
+    @FXML
+    private TableColumn<Part, Integer> partId;
+
+    @FXML
+    private TableColumn<Part, Integer> partLvl;
+
+    @FXML
+    private TableColumn<Part, String> partName;
+
+    @FXML
+    private TableColumn<Part, Double> partPrice;
+
+    @FXML
+    private TextField partsSearchField;
+
+    @FXML
+    private TableColumn<?, ?> productId;
+
+    @FXML
+    private TableColumn<?, ?> productLvl;
+
+    @FXML
+    private TableColumn<?, ?> productName;
+
+    @FXML
+    private TableColumn<?, ?> productPrice;
+
+    @FXML
+    private TextField productsSearchField;
+
+    @FXML
+    private TableView<?> tbParts;
+
+    @FXML
+    private TableView<?> tbProducts;
+
+    @FXML
+    protected void onExitButtonClick() {
+        System.exit(0);
+    }
+
+    @FXML
+    void onPartsAddButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onPartsDeleteButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onPartsModifyButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onProductsAddButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onProductsDeleteButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onProductsModifyButtonClick(ActionEvent event) {
+
+    }
 
 }
