97916f513b658ae5f62f7a379d20d74930f6edc4
James Dunaway <jamesdunaway95@gmail.com>
2023-10-05 18:52:25 -0700

    Changed everything in the Inventory class to static. The program should not require an inventory object to be created.

--- src/main/java/com/jd/inventorymanagement/Inventory.java
+++ src/main/java/com/jd/inventorymanagement/Inventory.java
@@ -7,20 +7,20 @@ import javafx.collections.ObservableList;
  * @author James Dunaway
  */
 public class Inventory {
-    private ObservableList<Part> allParts = FXCollections.observableArrayList();
-    private ObservableList<Product> allProducts = FXCollections.observableArrayList();
+    static ObservableList<Part> allParts = FXCollections.observableArrayList();
+    static ObservableList<Product> allProducts = FXCollections.observableArrayList();
 
     /**
      * @param newPart the newPart to add to inventory
      */
-    public void addPart(Part newPart) {
+    static void addPart(Part newPart) {
         allParts.add(newPart);
     }
 
     /**
      * @param newProduct the newProduct to add to inventory
      */
-    public void addProduct(Product newProduct) {
+    static void addProduct(Product newProduct) {
         allProducts.add(newProduct);
     }
 
@@ -28,7 +28,7 @@ public class Inventory {
      * @param partId the partId to lookup in inventory
      * @return Part, if exists.
      */
-    public Part lookupPart(Integer partId) {
+    static Part lookupPart(Integer partId) {
         /**
          * TODO: Add search function to find matching partId in allParts list. BST?
          */
@@ -39,7 +39,7 @@ public class Inventory {
      * @param partName the partName to lookup in inventory.
      * @return ObservableList with all parts matching partName.
      */
-    public ObservableList<Part> lookupPart(String partName) {
+    static ObservableList<Part> lookupPart(String partName) {
         /**
          * TODO: Add search function to find matching partName(s) in allParts list. BST?
          */
@@ -50,7 +50,7 @@ public class Inventory {
      * @param productId the productId to lookup in inventory.
      * @return Product, if exists.
      */
-    public Product lookupProduct(Integer productId) {
+    static Product lookupProduct(Integer productId) {
         /**
          * TODO: Add search function to find matching productId in allProducts list. BST?
          */
@@ -61,7 +61,7 @@ public class Inventory {
      * @param productName the productName to lookup in inventory.
      * @return ObservableList with all Product(s) matching productName.
      */
-    public ObservableList<Product> lookupProduct(String productName) {
+    static ObservableList<Product> lookupProduct(String productName) {
         /**
          * TODO: Add search function to find matching productName(s) in allProducts list. BST?
          */
@@ -72,7 +72,7 @@ public class Inventory {
      * @param index the index of the selectedPart to be updated.
      * @param selectedPart the selectedPart to replace the current one in inventory.
      */
-    public void updatePart(Integer index, Part selectedPart) {
+    static void updatePart(Integer index, Part selectedPart) {
         /**
          * TODO: Add update functionality, e.g. find the selected part at the provided index and replace it with the selectedPart.
          */
@@ -82,7 +82,7 @@ public class Inventory {
      * @param index the index of the selectedProduct to be updated.
      * @param selectedProduct the selectedProduct to replace the current one in inventory.
      */
-    public void updateProduct(Integer index, Product selectedProduct) {
+    static void updateProduct(Integer index, Product selectedProduct) {
         /**
          * TODO: Add update functionality, e.g. find the selected product at the provided index and replace it with the selectedProduct.
          */
@@ -95,7 +95,7 @@ public class Inventory {
      * @param selectedPart the part to delete from current inventory.
      * @return true if deleted, else, false.
      */
-    public boolean deletePart(Part selectedPart) {
+    static boolean deletePart(Part selectedPart) {
         return allParts.remove(selectedPart);
     }
 
@@ -106,7 +106,7 @@ public class Inventory {
      * @param selectedProduct the product to delete from current inventory.
      * @return true if deleted, else, false.
      */
-    public boolean deleteProduct(Product selectedProduct) {
+    static boolean deleteProduct(Product selectedProduct) {
         return allProducts.remove(selectedProduct);
     }
 
@@ -116,7 +116,7 @@ public class Inventory {
     /**
      * @return ObservableList of all parts in inventory.
      */
-    public ObservableList<Part> getAllParts() {
+    static ObservableList<Part> getAllParts() {
         return allParts;
     }
 
@@ -126,7 +126,7 @@ public class Inventory {
     /**
      * @return ObservableList of all products in inventory.
      */
-    public ObservableList<Product> getAllProducts() {
+    static ObservableList<Product> getAllProducts() {
         return allProducts;
     }
 }
